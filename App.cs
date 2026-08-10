8a77d12a31d138af895e975417d6f674028e8393
anton.sadchikov00@gmail.com <anton.sadchikov00@gmail.com>
2026-08-10 14:59:07 +0700

    1

--- App.cs
+++ App.cs
@@ -6,7 +6,7 @@ using STPLib.WebLog.Api;
 using STPLib.WebLog.Enums;
 using STPLib.WebLog.Pipelines;
 
-//test pipeline6
+//test pipeline7
 namespace LogPlgTest
 {
     [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
